7c836484fe371b12e9a7c6ecd3f1fcf0116cbd8b
akerfeels <netobalby@gmail.com>
2023-01-07 20:31:39 +0000

    refactor: move creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,10 +1,13 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 
+const makeSut = () : SignUpController => {
+    return new SignUpController();
+}
 describe('SignUp Controller' , () => {
     test('Should return 400 if no name is provided', () => {
         
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 // name: 'any_name',
@@ -22,7 +25,7 @@ describe('SignUp Controller' , () => {
 
     test('Should return 400 if no email is provided', () => {
         
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 name: 'any_name',
