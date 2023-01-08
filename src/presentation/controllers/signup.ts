a65cd1e580519aafda7ce4011e3cf2b2843a2efa
akerfeels <netobalby@gmail.com>
2023-01-08 10:36:19 +0000

    ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -29,6 +29,10 @@ export class SignUpController implements Controller {
                 return badRequest(new InvalidParamError('email'))
             }
 
+            if(httpRequest.body.password !== httpRequest.body.passwordConfirmation){
+                return badRequest(new InvalidParamError('passwordConfirmation'))
+            }
+
         }catch(error)
         {
             return serverError();
