581b372b1552ecea4c8214620ce940319e72e5e6
akerfeels <netobalby@gmail.com>
2023-01-08 09:08:11 +0000

    feat: added emailValidator call test

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { Http2ServerRequest } from 'http2'
 export class SignUpController implements Controller {
 
     private readonly emailValidator: EmailValidator
